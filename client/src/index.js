873e0a4f68fd5af631abfcf147d0d3e3aa99bcb6
zaksalazar <zsalazar.tv@gmail.com>
2023-03-04 11:25:47 -0800

    inital commit with starter code

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./App";
+
+createRoot(document.getElementById("root")).render(<App tab="home" />);
